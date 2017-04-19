f3e667fb75692e7f39321b9af766fd9657b418e3
alinton12 <lintonarron@gmail.com>
2017-04-18 23:34:26 -0400

    react-native-camera init

---
+++ camerahere/index.ios.js
@@ -0,0 +1,66 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ * @flow
+ */
+
+import React, { Component } from 'react';
+import {
+  AppRegistry,
+  StyleSheet,
+  Dimensions,
+  TouchableHighlight,
+  Text,
+  View,
+} from 'react-native';
+
+import Camera from 'react-native-camera';
+
+export default class camerahere extends Component {
+  clickedme(){
+    alert("was touched");
+  }
+  render() {
+    return (
+      <View style={styles.container}>
+        <Camera
+          ref={(cam) => {
+            this.camera = cam;
+          }}
+          style={styles.preview}
+          aspect={Camera.constants.Aspect.fill}>
+          <Text style={styles.capture} onPress={this.takePicture.bind(this)}>[CAPTURE]</Text>
+          <TouchableHighlight onPress={this.clickedme.bind(this)}>
+            <View style={{height:50, width:50, backgroundColor:"pink"}}></View>
+          </TouchableHighlight>
+        </Camera>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  },
+  instructions: {
+    textAlign: 'center',
+    color: '#333333',
+    marginBottom: 5,
+  },
+  preview: {
+    flex: 1,
+    justifyContent: 'flex-end',
+    alignItems: 'center'
+  },
+});
+
+AppRegistry.registerComponent('camerahere', () => camerahere);
