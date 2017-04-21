db2125204cad94f4dc19bf59a08a5de6827136c6
Kenny Rocha <kennyrocha@Kennys-MacBook-Air.local>
2017-04-21 00:26:00 -0400

    iOS camera test

---
+++ cameraiOS/index.ios.js
@@ -0,0 +1,75 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ * @flow
+ */
+
+import React, { Component } from 'react';
+import {
+  AppRegistry,
+  StyleSheet,
+  Text,
+  TouchableHighlight,
+  Dimensions,
+  View,
+} from 'react-native';
+import Camera from 'react-native-camera';
+
+
+export default class cameraiOS extends Component {
+  clickedme() {
+    alert('oh yes oh yes')
+  }
+  render() {
+    return (
+      <View style={styles.container}>
+        <Camera
+          ref={(cam) => {
+            this.camera = cam;
+          }}
+          style={styles.preview}
+          aspect={Camera.constants.Aspect.fill}>
+            <TouchableHighlight onPress={this.clickedme.bind(this)}>
+              <View style={{height:50,width:50,backgroundColor:'blue'}}></View>
+            </TouchableHighlight>
+        </Camera>
+      </View>
+    );
+  }
+
+  takePicture() {
+    const options = {};
+    //options.location = ...
+    this.camera.capture({metadata: options})
+      .then((data) => console.log(data))
+      .catch(err => console.error(err));
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  },
+    preview: {
+    flex: 1,
+    justifyContent: 'flex-end',
+    alignItems: 'center',
+    height: Dimensions.get('window').height,
+    width: Dimensions.get('window').width
+  },
+  instructions: {
+    textAlign: 'center',
+    color: '#333333',
+    marginBottom: 5,
+  },
+});
+
+AppRegistry.registerComponent('cameraiOS', () => cameraiOS);
